584cdac378ded023f235f4c291954a746012614c
TangJunLong <tangjunlong99@me.com>
2018-02-07 18:24:42 +0800

    删除多余文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,33 +3,46 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps : [
 
-    // First application
-    {
-      name      : 'Jkou_net',
-      script    : './bin/www',
-      env: {
-        COMMON_VARIABLE: 'true'
-      },
-      env_production : {
-        NODE_ENV: 'production'
+   apps : [
+
+      // First application
+      {
+          name      : 'jkou_net',
+          script    : './bin/www',
+          env: {
+              COMMON_VARIABLE: 'true'
+          },
+          env_production : {
+              NODE_ENV: 'production'
+          }
+      }
+    ],
+
+    /**
+     * Deployment section
+     * http://pm2.keymetrics.io/docs/usage/deployment/
+     */
+    deploy : {
+        production : {
+            user: 'tangjunlong',
+            host: '207.246.102.255',
+            ref: 'jkou_net/master',
+            repo: 'git@github.com:tangjunlong99/jkou_net.git',
+            path: '/home/tangjunlong/site/deploy',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+        }
+    },
+
+    dev : {
+      user : 'tangjunlong',
+      host : '207.246.102.255',
+      ref  : 'origin/master',
+      repo : 'git@github.com:tangjunlong99/jkou_net.git',
+      path : '/home/tangjunlong/site/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
       }
     }
-  ],
-
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy : {
-    production : {
-        user: 'root',
-        host: '207.246.102.255',
-        ref: 'origin/master',
-        repo: 'git@github.com:tangjunlong99/jkou_net.git',
-        path: '/home/root/www/production',
-        'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
 };
--- test2.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('you are so good\n');
-}).listen(2000,"127.0.0.1");
-console.log('http://127.0.0.1:2000/');
