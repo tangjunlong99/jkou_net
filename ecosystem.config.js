e189084b763cf0b4dee04fca1a9421166e9dc3ab
TangJunLong <tangjunlong99@me.com>
2018-02-07 17:14:39 +0800

    更新ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,13 +23,13 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
   deploy : {
-      production : {
-        user : 'root',
-        host : '207.246.102.255',
-        ref  : 'origin/master',
-        repo : 'git@github.com:tangjunlong99/jkou_net.git',
-        path : '/home/root/www/production',
-        'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-      }
+    production : {
+        user: 'root',
+        host: '207.246.102.255',
+        ref: 'origin/master',
+        repo: 'git@github.com:tangjunlong99/jkou_net.git',
+        path: '/home/root/www/production',
+        'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
     }
+  }
 };
